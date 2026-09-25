b6a2613ce9958c6de0f9ae68d77b589367ff15df
thunder <aoki.eita1130@gmail.com>
2026-09-25 10:25:35 +0900

    Allow ranking scores beyond ten stage clears

--- game/voldecade/sw.js
+++ game/voldecade/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "voldecade-static-a58ee5db044e4359";
+const CACHE = "voldecade-static-a6968d4dcfd763c6";
 const SHELL_ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./ai-worker.js?v=37e3bb37dd1553f3", "./manifest.webmanifest", "./assets/design/fabicon/fabicon.png"];
 const MAX_RUNTIME_ASSETS = 96;
 
